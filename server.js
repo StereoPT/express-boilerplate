3ab6605cda4f8b14e062b40a6386fa4f9764da5d
GuidoSP <stereopt@gmail.com>
2020-11-13 17:47:55 +0000

    Main Route

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log("[Express.js Boilerplate]");
+const express = require('express');
+
+require('dotenv').config();
+
+const server = express();
+
+console.log('[Express.js Boilerplate]');
+
+server.get('/', (req, res) => {
+  res.json({
+    message: 'Express.js Boilerplate',
+  });
+});
+
+const port = process.env.PORT || 1337;
+server.listen(port, () => {
+  console.log(`Listening at: http://localhost:${port}/`);
+});
